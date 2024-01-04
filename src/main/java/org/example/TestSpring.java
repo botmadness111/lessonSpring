b9ef0d91d9afced92b288ed1a9d699f5bffe04dd
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-01-04 15:26:54 +0300

    Dependency Injection_1
    //Добавление зависимостей. Создаю bean для MusicPlayer в xml и там в конструктор передаю bean у Music
    
    Тем самым избавляюсь от создания вручную объектов классов.

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,10 +8,7 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
-
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
