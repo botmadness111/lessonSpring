f39bbc973d083ea80f9344ea209d78407cf91aa5
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-01-05 19:23:32 +0300

    Annotations
    -@Qualifier

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,21 +1,22 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public void setMusic(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public void setMusic(@Qualifier("rockMusic") Music music1, @Qualifier("jazzMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return rockMusic.getSong();
+        return music1.getSong() + ", " + music2.getSong();
     }
 }
